042cdac84aa717f1d753b3f92893c018d6c5794c
DESKTOP-4PFVMS4\Shimanto Ahmed <afser@parallaxlogic.com>
2021-02-24 01:20:04 +0600

    bloc implementation

--- lib/ui/home/HomePage.dart
+++ lib/ui/home/HomePage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learning_app_flutter/ui/home/bloc/home_bloc.dart';
+import 'package:learning_app_flutter/ui/home/bloc/home_state.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,20 +9,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  HomeBloc _homeBloc;
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+
+    //Close the Stream Sink when the widget is disposed
+    _homeBloc?.close();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // return Container(
-    //   margin: EdgeInsets.all(20),
-    //   padding: EdgeInsets.all(10),
-    //   // color: Colors.grey[200],
-    //   child: ListView.builder(
-    //     shrinkWrap: true,
-    //     physics: ScrollPhysics(),
-    //     itemCount: _queAnsList.length,
-    //     itemBuilder: (context, index) {
-    //       return questionListItem(index);
-    //     },
-    //   ),
-    // );
+    //Initializing Bloc Sink by using BlocProvider
+    _homeBloc = BlocProvider.of<HomeBloc>(context);
+
+    return BlocBuilder<HomeBloc, HomeState>(
+        builder: (context, state) {
+          return Container(
+            margin: EdgeInsets.all(20),
+            padding: EdgeInsets.all(10),
+            // color: Colors.grey[200],
+            child: ListView.builder(
+              shrinkWrap: true,
+              physics: ScrollPhysics(),
+              itemCount: ((state as ListState).userList.length),
+              itemBuilder: (context, index) {
+                return Container();
+              },
+            ),
+          );
+        }
+
+    );
   }
 }
--- lib/ui/home/bloc/home_bloc.dart
+++ lib/ui/home/bloc/home_bloc.dart
@@ -2,25 +2,34 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:learning_app_flutter/ui/home/bloc/home_event.dart';
 import 'package:learning_app_flutter/ui/home/bloc/home_state.dart';
 
-// class HomeBloc extends Bloc<HomeEvent, HomeState> {
-//
-//
-//   @override
-//   // TODO: implement initialState
-//   HomeState get initialState => CheckListEmpty();
-//
-//   @override
-//   Stream<ApplicationState> mapEventToState(ApplicationEvent event) async* {
-//     if (event is FetchApplication) {
-//       yield ApplicationLoading();
-//
-//       try {
-//         final Application application =
-//         await applicationRepository.fetchApplication();
-//         yield ApplicationLoaded(application: application);
-//       } catch (_) {
-//         ApplicationError();
-//       }
-//     }
-//   }
-// }
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  HomeBloc(HomeState initialState) : super(initialState);
+
+
+
+  @override
+  // TODO: implement initialState
+  HomeState get initialState {
+    CheckListEmpty(isListEmpty: false);
+    ListState(userList: []);
+  }
+
+  @override
+  Stream<HomeState> mapEventToState(HomeEvent event) async* {
+    if (event is IsListEmptyEvent) {
+      bool isEmpty = (state as CheckListEmpty).isListEmpty;
+      yield CheckListEmpty();
+
+      // try {
+      //   final Application application =
+      //   await applicationRepository.fetchApplication();
+      //   yield ApplicationLoaded(application: application);
+      // } catch (_) {
+      //   ApplicationError();
+      // }
+    }
+    else if(event is GetListEvent){
+
+    }
+  }
+}
